69ab189b459b5a2a6ddc1d5d2088fc0d46f2c43b
Ruel Tingling <rvtingling@yahoo.com>
2019-10-09 19:35:06 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,23 @@
 # Write a speak_to_grandma method.
 
+
+def speak_to_grandma(phrase)
+  
+if phrase != phrase.upcase
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif phrase == phrase.upcase
+  puts "NO, NOT SINCE 1938!"
+elsif phrase == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!" 
+
+end 
+
+
+
+
+
+
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
