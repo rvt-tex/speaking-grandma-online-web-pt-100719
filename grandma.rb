70a29d452ed0e6bc96b7c849f1f37bc68d2aca80
Ruel Tingling <rvtingling@gmail.com>
2019-10-11 20:43:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,10 +5,12 @@ def speak_to_grandma(phrase)
   
 if phrase != phrase.upcase
   return "HUH?! SPEAK UP, SONNY!"
-elsif phrase == phrase.upcase
+elsif phrase == "I LOVE YOU GRANDMA!"
   return "NO, NOT SINCE 1938!"
 elsif phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!" 
+  return "I LOVE YOU TOO PUMPKIN!" 
+else 
+  return "NO, NOT SINCE 1938!"
 end
 end 
 
