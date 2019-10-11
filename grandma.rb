1fa46f3f63e0a17fb093e6896d8b6c1b9c12ee75
Ruel Tingling <rvtingling@gmail.com>
2019-10-11 20:55:03 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,12 +4,12 @@
 def speak_to_grandma(phrase)
   
 if phrase != phrase.upcase
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase == phrase.upcase
-  puts "NO, NOT SINCE 1938!"
+  return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!" 
-
+  return "I LOVE YOU TOO PUMPKIN!"
+else 
+  return "NO, NOT SINCE 1938!"
+end
 end 
 
 
